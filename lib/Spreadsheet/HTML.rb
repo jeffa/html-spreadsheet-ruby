b153903c6ac1a72db2f7fa9b35b83ae8b8eee59e
jeffa <jeffa@unlocalhost.com>
2015-09-14 17:06:20 -0400

    added bare minimum generator()

--- lib/Spreadsheet/HTML.rb
+++ lib/Spreadsheet/HTML.rb
@@ -2,8 +2,22 @@ require "Spreadsheet/HTML/version"
 
 module Spreadsheet
     class HTML
-        def generate(data)
-            data
+
+        def generate( data )
+
+            cdata = '<table>'
+
+            data.each do |row|
+                cdata += '<tr>'
+                row.each do |col|
+                    cdata += '<td>' + col + '</td>'
+                end
+                cdata += '</tr>'
+            end
+
+            cdata += '/<table>'
+            return cdata
         end
+
     end
 end
--- t/02-run.rb
+++ t/02-run.rb
@@ -4,7 +4,10 @@ require "Spreadsheet/HTML.rb"
 class Test_Run < Test::Unit::TestCase
 
   def test_run
-    assert_equal( Spreadsheet::HTML.new.generate( 2 ), 2 )
+    assert_equal(
+        Spreadsheet::HTML.new.generate( Array[['a','b'], ['c','d']] ),
+        '<table><tr><td>a</td><td>b</td></tr><tr><td>c</td><td>d</td></tr>/<table>',
+    )
   end
 
 end
