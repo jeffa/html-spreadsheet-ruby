bad233362e3785c9b43c954002df512c1857346c
jeffa <jeffa@unlocalhost.com>
2015-09-14 17:38:07 -0400

    no trailing chars for old ruby?

--- t/02-run.rb
+++ t/02-run.rb
@@ -7,12 +7,12 @@ class Test_Run < Test::Unit::TestCase
 
     assert_equal(
         Spreadsheet::HTML.new.generate( Array[[1,2], [3,4]] ),
-        '<table><tr><td>1</td><td>2</td></tr><tr><td>3</td><td>4</td></tr>/<table>',
+        '<table><tr><td>1</td><td>2</td></tr><tr><td>3</td><td>4</td></tr>/<table>'
     )
 
     assert_equal(
         Spreadsheet::HTML.new.generate( Array[['a','b'], ['c','d']] ),
-        '<table><tr><td>a</td><td>b</td></tr><tr><td>c</td><td>d</td></tr>/<table>',
+        '<table><tr><td>a</td><td>b</td></tr><tr><td>c</td><td>d</td></tr>/<table>'
     )
 
   end
