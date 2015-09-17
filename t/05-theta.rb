2e78f60945b30252960b282d4945d2f8c97705a2
jeffa <jeffa@unlocalhost.com>
2015-09-17 15:48:26 -0700

    3/7 thetas implemented

--- t/05-theta.rb
+++ t/05-theta.rb
@@ -3,6 +3,41 @@ require "Spreadsheet/HTML.rb"
 
 class Test_Theta < Test::Unit::TestCase
 
+  def test_0flip
+
+    data = Array[
+        %w(header1 header2 header3 header4 ),
+        %w(foo1 bar1 baz1 qux1),
+        %w(foo2 bar2 baz2 qux2),
+        %w(foo3 bar3 baz3 qux3),
+        %w(foo4 bar4 baz4 qux4)
+    ]
+
+    html = '<table><tr><th>header4</th><th>header3</th><th>header2</th><th>header1</th></tr><tr><td>qux1</td><td>baz1</td><td>bar1</td><td>foo1</td></tr><tr><td>qux2</td><td>baz2</td><td>bar2</td><td>foo2</td></tr><tr><td>qux3</td><td>baz3</td><td>bar3</td><td>foo3</td></tr><tr><td>qux4</td><td>baz4</td><td>bar4</td><td>foo4</td></tr></table>';
+
+    assert_equal(
+        html,
+        Spreadsheet::HTML.new( 'data' => data, 'flip' => 1 ).generate(),
+        "via constructor only"
+    )
+    assert_equal(
+        html,
+        Spreadsheet::HTML.new( 'data' => data ).generate( 'flip' => 1 ),
+        "via constructor and method"
+    )
+    assert_equal(
+        html,
+        Spreadsheet::HTML.new.generate( data, 'flip' => 1 ),
+        "with array ref"
+    )
+    assert_equal(
+        html,
+        Spreadsheet::HTML.new.generate( 'data' => data, 'flip' => 1 ),
+        "named args only"
+    )
+
+  end
+
   def test_90
 
     data = Array[
@@ -153,7 +188,7 @@ class Test_Theta < Test::Unit::TestCase
         %w(foo4 bar4 baz4 qux4)
     ]
 
-    html = '<table><tr><th>header1</th><th>header2</th><th>header3</th><th>header4</th></tr><tr><td>foo1</td><td>bar1</td><td>baz1</td><td>qux1</td></tr><tr><td>foo2</td><td>bar2</td><td>baz2</td><td>qux2</td></tr><tr><td>foo3</td><td>bar3</td><td>baz3</td><td>qux3</td></tr><tr><td>foo4</td><td>bar4</td><td>baz4</td><td>qux4</td></tr></table>';
+    html = '<table><tr><th>header4</th><td>qux1</td><td>qux2</td><td>qux3</td><td>qux4</td></tr><tr><th>header3</th><td>baz1</td><td>baz2</td><td>baz3</td><td>baz4</td></tr><tr><th>header2</th><td>bar1</td><td>bar2</td><td>bar3</td><td>bar4</td></tr><tr><th>header1</th><td>foo1</td><td>foo2</td><td>foo3</td><td>foo4</td></tr></table>';
 
     assert_equal(
         html,
