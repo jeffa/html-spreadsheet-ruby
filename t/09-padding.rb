7d20360590f3420b46c857135a060fb18f1f25d0
jeffa <jeffa@unlocalhost.com>
2015-10-28 19:53:20 -0400

    implemented empty param

--- t/09-padding.rb
+++ t/09-padding.rb
@@ -13,11 +13,11 @@ class Test_Padding < Test::Unit::TestCase
 
     gen = Spreadsheet::HTML.new( 'data' => data )
 
-#    assert_equal(
-#        '<table><tr><th>header1</th><th>header2</th><th>header3</th></tr><tr><td>foo1</td><td>bar1</td><td>&nbsp;</td></tr><tr><td>foo2</td><td>bar2</td><td>&nbsp;</td></tr></table>',
-#        gen.generate(),
-#        "empty values are padded with default"
-#    )
+    assert_equal(
+        '<table><tr><th>header1</th><th>header2</th><th>header3</th></tr><tr><td>foo1</td><td>bar1</td><td>&nbsp;</td></tr><tr><td>foo2</td><td>bar2</td><td>&nbsp;</td></tr></table>',
+        gen.generate(),
+        "empty values are padded with default"
+    )
 
   end
 
